b2ae8ab406fee5225467c0ced4d3e364ffbb8d94
Adam Bergman <adam@fransvilhelm.com>
2021-04-12 20:36:29 +0200

    Update templates

--- src/templates.ts
+++ src/templates.ts
@@ -3,13 +3,13 @@ import Handlebars from 'handlebars';
 import { join } from 'path';
 
 export function getHtmlTemplates() {
-  const readFile = (name: string) => {
-    return readFileSync(join(__dirname, './templates', name), 'utf-8');
+  const readTemplate = (name: string) => {
+    return readFileSync(join(__dirname, '../templates', name), 'utf-8');
   };
 
   return {
-    list: Handlebars.compile(readFile('list.hbs')),
-    notFound: Handlebars.compile(readFile('not-found.hbs')),
-    error: Handlebars.compile(readFile('error.hbs')),
+    list: Handlebars.compile(readTemplate('list.hbs')),
+    notFound: Handlebars.compile(readTemplate('not-found.hbs')),
+    error: Handlebars.compile(readTemplate('error.hbs')),
   };
 }
